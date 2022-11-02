97da7707b542fa5beb8088fa017d0d988057f72b
Jon Pena <iamjonntech@gmail.com>
2022-11-01 22:05:07 -0700

    fixing header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,35 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+// import { logout, reset } from "../features/auth/authSlice";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        <li>
+          <button className="btn">
+            <FaSignOutAlt /> Logout
+          </button>
+        </li>
+
+        <>
+          <li>
+            <Link to="/profile">
+              <FaSignInAlt /> Profile
+            </Link>
+          </li>
+          <li>
+            <Link to="/Feed">
+              <FaUser /> Feed
+            </Link>
+          </li>
+        </>
+      </ul>
+    </header>
+  );
 }
 export default Header;
